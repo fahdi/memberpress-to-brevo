f9d5b2c5ceee5f800a6b33057fac2dbfc5bd02fb
isupercoder <info@fahdmurtaza.com>
2024-01-02 17:41:33 +0500

    Initialize MemberPress to Brevo Integration Plugin
    Set up the basic plugin file with the WordPress plugin header. This includes the plugin's name, description, version, author name, and URI. This initial setup is essential for WordPress to recognize and handle the plugin correctly.

---
+++ memberpress-to-brevo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: MemberPress to Brevo Integration
+ * Plugin URI: https://app.codeable.io/tasks/new?preferredContractor=49688
+ * Description: Integrates MemberPress with Brevo (SendInBlue).
+ * Version: 1.0
+ * Author: Fahad Murtaza
+ * Author URI: https://app.codeable.io/tasks/new?preferredContractor=49688
+ */
